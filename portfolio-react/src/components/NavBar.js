b3164d60d0e669fed32b17d4a1b9a8587bb9ad40
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-01-25 21:38:34 +0500

    Added some Css

--- portfolio-react/src/components/NavBar.js
+++ portfolio-react/src/components/NavBar.js
@@ -1,12 +1,16 @@
 import {NavBar,Container} from "react-bootstrap";
 import {useState , useEffect} from "react";
+import logo from '../assets/images/logo.svg';
+import navIcon1 from '../assets/images/nav-icon1.svg';
+import navIcon2 from '../assets/images/nav-icon2.svg';
+import navIcon3 from '../assets/images/nav-icon3.svg';
 
 export const NavBar = () => {
     const {ActiveLink , setActiveLink} = useState('home');
     const {scrolled , setScolled} = useState(false);
     useEffect(() => {
         const onScroll = () => {
-            if(window.crollY > 50){
+            if(window.scrollY > 50){
                 setScolled(true);
             }else{
                 setScolled(false);
@@ -18,13 +22,13 @@ export const NavBar = () => {
     },[]);
 
     const onUpdateActiveLink = () => {
-        setActiveLink
+        setActiveLink(value);
     }
     return (
         <NavBar expand="lg" className={scrolled ? "scrolled" : ""}>
             <Container>
                 <NavBar.Brand href="#home">
-                    <img src={''} alt="logo"/>
+                    <img src={logo} alt="logo"/>
                 </NavBar.Brand>
                 <NavBar.Toggle aria-controls="basic-navbar-nav">
                     <span className="navbar-toggler-icon"></span>
@@ -37,9 +41,9 @@ export const NavBar = () => {
                     </Nav>
                     <span className="navbar-text">
                         <div className="social-icon">
-                            <a href = "#"><img src={''} alt=" "></img></a>
-                            <a href = "#"><img src={''} alt=" "></img></a>
-                            <a href = "#"><img src={''} alt=" "></img></a>
+                            <a href = "#"><img src={navIcon1} alt=" "></img></a>
+                            <a href = "#"><img src={navIcon2} alt=" "></img></a>
+                            <a href = "#"><img src={navIcon3} alt=" "></img></a>
                         </div>
                         <button className="vvd" onClick={()=>console.log("connect")}><span>Let's Connect</span></button>
                     </span>
