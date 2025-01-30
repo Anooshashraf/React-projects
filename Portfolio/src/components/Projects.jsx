090d99dc1cd9f8a94e873dcbc98d0c0be45c3572
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-01-30 21:40:09 +0500

    Project CSS added

--- Portfolio/src/components/Projects.jsx
+++ Portfolio/src/components/Projects.jsx
@@ -1,5 +1,10 @@
-import { Container } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
+import colorSharp2 from "../assets/images/color-sharp2.png";
+import projImg1 from "../assets/images/project-img1.png";
+import projImg2 from "../assets/images/project-img2.png";
+import projImg3 from "../assets/images/project-img3.png";
+import {Col,Row,Container,Nav,Tab} from "react-bootstrap";
+
 
 export const Projects = () => {
 
@@ -7,49 +12,55 @@ const projects = [
     {
         title: "Buisness Startup" ,
         description: "Design & development",
-        imgUrl: projectImg1
+        imgUrl: projImg1
     },
     {
-        title: "" ,
+        title: "Buisness Start-up" ,
         description: "Design & development",
-        imgUrl: projectImg1
-    },
-    {
-        title: "Buisness Startup" ,
-        description: "Design & development",
-        imgUrl: projectImg1
+        imgUrl: projImg2
     },
     {
         title: "Buisness Startup" ,
         description: "Design & development",
-        imgUrl: projectImg1
+        imgUrl: projImg3
+    },
+    {
+        title: "Buisness Startup" ,
+        description: "Design & development",
+        imgUrl: projImg1
+    }
+    ,
+    {
+        title: "Buisness Startup" ,
+        description: "Design & development",
+        imgUrl: projImg2
     }
 
 ];
 
     return (
-        <section className="project" id="project">
+        <section className="project" id="projects">
             <Container>
                 <Row>
-                    <Col>
+                    <Col size={12}>
                         <h2>Projects</h2>
                         <p>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.
                         </p>
-                        <Tab.container id="projects-tabs" defaultActiveKey="first">
+                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
                             <Nav variant="pills" defaultActiveKey="/home">
                                 <Nav.Item eventkey="first">
-                                    <Nav.link>Tab one</Nav.link>
+                                    <Nav.Link>Tab one</Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item eventkey="second">
-                                    <Nav.link>Second one</Nav.link>
+                                    <Nav.Link>Second one</Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item eventkey="third">
-                                    <Nav.link>Third</Nav.link>
+                                    <Nav.Link>Third</Nav.Link>
                                 </Nav.Item>
                             </Nav>
-                            <Tab.content>
-                                <Tab.pane eventkey="first">
+                            <Tab.Content>
+                                <Tab.Pane eventkey="first">
                                     <Row>
                                         {
                                             projects.map((project,index)=>{
@@ -61,15 +72,15 @@ const projects = [
                                             })
                                         }
                                     </Row>
-                                </Tab.pane>
-                                <Tab.pane eventkey="second">Loren Ipsum</Tab.pane>
-                                <Tab.pane eventkey="third">Loren Ipsum</Tab.pane>
-                            </Tab.content>
-                        </Tab.container>
+                                </Tab.Pane>
+                                <Tab.Pane eventkey="second">Loren Ipsum</Tab.Pane>
+                                <Tab.Pane eventkey="third">Loren Ipsum</Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
                     </Col>
                 </Row>
             </Container>
-            <img className="background-image-right" src="" alt="" />
+            <img className="background-image-right" src={colorSharp2} alt="" />
         </section>
     )
 }
