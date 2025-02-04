5020f9d80cd29464a60ce89b0b85843abf28730a
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-02-04 21:01:54 +0500

    new updates

--- Portfolio/src/components/MailChimpForm.jsx
+++ Portfolio/src/components/MailChimpForm.jsx
@@ -1,5 +1,6 @@
 import MailchimpSubscribe from "react-mailchimp-subscribe";
 import { Newsletter } from "./Newsletter";
+import process from 'process';
 
 export const MailchimpForm = () => {
   const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
@@ -17,5 +18,5 @@ export const MailchimpForm = () => {
         )}
         />
     </>
-  )
-}
+  );
+};
