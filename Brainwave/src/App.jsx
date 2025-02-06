31db6490ae163fd1dace845f43e71466df01eb24
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-02-06 21:01:47 +0500

    first button component

--- Brainwave/src/App.jsx
+++ Brainwave/src/App.jsx
@@ -5,10 +5,9 @@ const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">
-        hello world!
       </h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">Something</Button>
+        <Button className="mt-10" >Something</Button>
       </div>
       <ButtonGradient/>
     </>
