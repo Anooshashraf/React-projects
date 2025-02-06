7504484caa06d1f1e30248fd2c837373cc525584
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-02-06 20:36:20 +0500

    starting project

--- Brainwave/src/App.jsx
+++ Brainwave/src/App.jsx
@@ -1,9 +1,17 @@
-function App() {
+import ButtonGradient from "./assets/svg/ButtonGradient (1)";
+import Button from "./components/Button";
+const App = () => {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        hello world!
+      </h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">Something</Button>
+      </div>
+      <ButtonGradient/>
+    </>
   );
   
 };
