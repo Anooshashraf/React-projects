255568455b7d182df2737eaad389de0c5d899aeb
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-02-07 19:54:47 +0500

    header and section changes

--- Brainwave/src/App.jsx
+++ Brainwave/src/App.jsx
@@ -1,14 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient (1)";
-import Button from "./components/Button";
+import Header from "./components/Header";
 const App = () => {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      </h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" >Something</Button>
+        <Header/>
       </div>
+      
       <ButtonGradient/>
     </>
   );
