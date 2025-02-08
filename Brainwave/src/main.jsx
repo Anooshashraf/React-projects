8751eb1ff68845a3aeec4867c6ec6f26a4117c0f
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-02-09 00:31:00 +0500

    main.jsx initiation

--- Brainwave/src/main.jsx
+++ Brainwave/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react"; // Import React for JSX
+import { createRoot } from "react-dom/client"; // Import createRoot from react-dom/client
+import { BrowserRouter as Router } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+import App from "./App.jsx";
+
+// Create a root and render the app
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
