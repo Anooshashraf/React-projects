821da49fe3967d3f28b04e56fd9c4649d6c613b0
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-02-09 00:58:29 +0500

    button

--- Brainwave/src/App.jsx
+++ Brainwave/src/App.jsx
@@ -1,13 +1,19 @@
+import ButtonGradiant from "./assets/svg/ButtonGradient";
+import { Button } from "./components/Button.jsx";
 
-function App() {
+const App = () => {
 
   return (
     <>
       <h1 className='text-3xl font-bold underline'>
         Hello world!
       </h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">Something</Button>
+      </div>
+      <ButtonGradiant />
     </>
   )
 }
 
-export default App
+export default App;
