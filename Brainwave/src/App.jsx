d349ab4ed773b64bf6e5cbcbde428392ae49f29e
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-02-09 12:35:32 +0500

    making essential files

--- Brainwave/src/App.jsx
+++ Brainwave/src/App.jsx
@@ -1,19 +1,31 @@
-import ButtonGradiant from "./assets/svg/ButtonGradient";
-import { Button } from "./components/Button.jsx";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Services from "./components/Services";
+import Button from "./components/Button";
 
 const App = () => {
-
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>
-        Hello world!
-      </h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">Something</Button>
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
-      <ButtonGradiant />
+
+      <ButtonGradient />
     </>
-  )
-}
+  );
+};
 
 export default App;
--- Brainwave/src/main.jsx
+++ Brainwave/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react"; // Import React for JSX
 import { createRoot } from "react-dom/client"; // Import createRoot from react-dom/client
 import { BrowserRouter as Router } from "react-router-dom";
-
 import "./index.css";
 import App from "./App.jsx";
 
