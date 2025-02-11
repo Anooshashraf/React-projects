88c1522d61d34d1df73eb0615681d3e0ccf75fd5
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-02-11 19:31:52 +0500

    complete navigation

--- Brainwave/src/components/Header.jsx
+++ Brainwave/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
+import { useLocation } from "react";
+import Button from "./Button";
 
 const Header = () => {
   return (
-    <div className='fixed top-5 z-50 bg-n-8/90 backdrop-blur-sm border-b
+    <div className='fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b
      border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm'>
       <div className='flex items-center  px-5 lg:px-7.5 xl:px-10 max-lg:py-4'>
         <a className="block w-[12rem] xl:mr-8" href="#hero" >
@@ -13,12 +15,19 @@ const Header = () => {
         lg:flex lg:mx-auto lg:bg-transparent">
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item)=>(
-              <a key={item.id} href={item.url}>
+              <a key={item.id} href={item.url} 
+              className={`block relative font-code text-2xl uppercase text-n-1 transition-colors
+              hover:text-color-1 ${item.onlyMobile ? 'lg:hidden' : ''} px-6 py-6 md:py-8 lg:-mr-0.25 
+              lg:text-xs lg:font-semibold`}>
                 {item.title}
               </a>
             ))}
           </div>
         </nav>
+        <a href="#signup" className={`button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block`}> 
+        New Account
+        </a>
+        <Button className="hidden lg:flex" href="#login">Sign in</Button>
       </div>
     </div>
   );
