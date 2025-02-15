a34491e2a2c103be9dfdc10af6252a09884aa951
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-02-15 17:15:23 +0500

    complete header component

--- Brainwave/src/components/Header.jsx
+++ Brainwave/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { brainwave } from "../assets";
+import { disablePageScroll , enablePageScroll} from "scroll-lock";
 import { navigation } from "../constants";
 import { useState } from "react";
 import Button from "./Button";
@@ -13,12 +14,17 @@ const Header = () => {
   const toggleNavigation = () => {
     if(openNavigation){
       setOpenNavigation(false);
+      enablePageScroll();
     }else{
       setOpenNavigation(true);
+      disablePageScroll();
     };
   };
 
   const handleClick = () =>{
+    if(!openNavigation) return;
+
+    enablePageScroll();
     setOpenNavigation(false);
   };
   
