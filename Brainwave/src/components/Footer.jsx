9c90318e861d5c57e749c4e3e2637724473a61e6
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-02-21 16:29:43 +0500

    Footer changes

--- Brainwave/src/components/Footer.jsx
+++ Brainwave/src/components/Footer.jsx
@@ -1,13 +1,26 @@
 import Section from "./Section";
+import { socials } from "../constants";
 const Footer = () => {
   return (
     <Section crosses className={`!px-0 !py-10 `}>
       <div className="container flex sm:justify-between justify-center
       items-center gap-10 max-sm:flex-col">
-        {new Date().getFullYear()} All Rights reserved.
+        <p className="caption text-n-4 lg:block">
+          {new Date().getFullYear()} All Rights reserved.
+
+        </p>
+        <ul className="flex gap-5 flex-wrap">
+          {socials.map((items)=>(
+            
+            <a href={items.url} key={items.id} >
+              <img src={items.iconUrl} width={16} height={16} alt={items.title} />
+            </a>
+            
+          ))}
+        </ul>
       </div>
     </Section>
   )
-}
+};
 
 export default Footer;
