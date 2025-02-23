e6b2179119f8b2350dd377a940339238754047d5
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-02-23 22:53:06 +0500

    logo changed in portfolio

--- Brainwave/src/components/Pricing.jsx
+++ Brainwave/src/components/Pricing.jsx
@@ -10,7 +10,7 @@ const Pricing = () => {
       <div className="container relative z-2">
         <div className="hidden relative justify-center mb-[6.5rem] lg:flex">
           <img 
-            src={smallSphere} className="relative z-1" width={255} height={255} alt="sphere"
+            src={smallSphere} className="relative" width={255} height={255} alt="sphere"
           />
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 w-[60rem] -translate-y-1/2 pointer-events-none">
             <img
