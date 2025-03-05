86da7b7d9d81b70e89dfa2e24a9380cd22ae20cc
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-03-05 23:25:16 +0500

    ..

--- Gpt3/src/containers/header/Header.jsx
+++ Gpt3/src/containers/header/Header.jsx
@@ -6,10 +6,10 @@ const Header = () => {
   return (
     <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
-        <h1 className="gradient__text">
+        <h1>
         Let’s Build Something amazing with GPT-3 OpenAI
         </h1>
-        <p className="gradient__text">
+        <p>
         Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. 
         Indulgence way everything joy alteration boisterous the attachment. 
         Party we years to order allow asked of.
