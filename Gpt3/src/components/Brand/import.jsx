75970e75ce6d0811f7c2ace4dd2f1ea604a60f27
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-03-08 11:29:46 +0500

    .. brand

--- Gpt3/src/components/Brand/import.jsx
+++ Gpt3/src/components/Brand/import.jsx
@@ -1,13 +1,13 @@
-import google from '../../assets/google';
-import slack from '../../assets/slack';
-import altassian from '../../assets/altassian';
-import dropbox from '../../assets/dropbox';
-import shopify from '../../assets/shopify';
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/shopify.png';
 
 export{
     google ,
     slack , 
-    altassian ,
+    atlassian ,
     dropbox ,
     shopify
 }
