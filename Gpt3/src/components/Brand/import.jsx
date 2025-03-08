dda5fc445d3afa00b36c02c74c65da959414e2fe
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-03-08 10:42:18 +0500

    import

---
+++ Gpt3/src/components/Brand/import.jsx
@@ -0,0 +1,13 @@
+import google from '../../assets/google';
+import slack from '../../assets/slack';
+import altassian from '../../assets/altassian';
+import dropbox from '../../assets/dropbox';
+import shopify from '../../assets/shopify';
+
+export{
+    google ,
+    slack , 
+    altassian ,
+    dropbox ,
+    shopify
+}
